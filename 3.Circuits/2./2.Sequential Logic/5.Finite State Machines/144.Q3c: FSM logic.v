f6eef7ac98f4b15f569885dc3dbea361ce910de2
Jun-recording <132544265+Jun-recording@users.noreply.github.com>
2024-02-07 03:36:51 +0900

    Create 144.Q3c: FSM logic.v

---
+++ 3.Circuits/2./2.Sequential Logic/5.Finite State Machines/144.Q3c: FSM logic.v
@@ -0,0 +1,11 @@
+module top_module (
+    input clk,
+    input [2:0] y,
+    input x,
+    output Y0,
+    output z
+);	
+    assign Y0 = ((y[2]|y[0])&~x) | (~y[2]&~y[0]&x);
+    assign z = y==3'b100 || y==3'b011;
+
+endmodule
